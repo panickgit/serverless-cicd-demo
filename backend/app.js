4bc03ae9c4a7339452000dc5a1b97f7a0ca4d5c9
Henry Suryawirawan <hsuryawirawan@google.com>
2019-01-24 10:25:02 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 app.get('/products', (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  
+
   let products = [
     {
       name: 'Striped skirt',
